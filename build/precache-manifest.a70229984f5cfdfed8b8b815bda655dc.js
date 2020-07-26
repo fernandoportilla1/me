80f5657b4a0c62e0be730717c29649b0e077cd4a
Fernando Portilla <fernandoportilla1@gmail.com>
2020-07-25 23:34:19 -0500

    travis

---
+++ build/precache-manifest.a70229984f5cfdfed8b8b815bda655dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc3b1358c65b2f79e043e229a4761ed7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e9183faa0caa2fe13f58",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "60c8671605d328ee1b7b",
+    "url": "./static/js/2.edc2f1fe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.edc2f1fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9183faa0caa2fe13f58",
+    "url": "./static/js/main.d00a2b5b.chunk.js"
+  },
+  {
+    "revision": "5a9ca95dfee7036cfe1b",
+    "url": "./static/js/runtime-main.6872adf3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
